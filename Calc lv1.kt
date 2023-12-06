b268a795fa9ec115a75d59feb250f2d990e8de73
LSW990918 <150113407+LSW990918@users.noreply.github.com>
2023-12-06 13:21:16 +0900

    Add files via upload

--- Calc lv1.kt
+++ Calc lv1.kt
@@ -8,23 +8,31 @@ fun main() {
     println("두번째 숫자를 입력하시오")
     var num2 = readln().toDouble()
 
-    while (true) {
+    var calc = Calculator(num1 , op, num2)
+    calc.operation()
+}
+
+class Calculator(num1: Double, op:String, num2: Double) {
+    var num1:Double = num1
+    var op:String = op
+    var num2:Double = num2
+    init {
+    }
+    fun operation() {
+        println("계산 진행 중!")
         if (op == "+") {
-            println(num1 + num2)
-            break
-        } else if (op == "-") {
-            println(num1 - num2)
-            break
-        } else if (op == "*") {
-            println(num1 * num2)
-            break
-        } else if (op == "/") {
-            println(num1 / num2)
-            break
-        } else {
-            println("기호를 다시 입력해주세요")
-            op = readln()
-            continue
+            println("${num1} ${op} ${num2} = ${num1+num2}")
         }
+        else if(op == "-" ) {
+            println("${num1} ${op} ${num2} = ${num1-num2}")
+        }
+        else if(op == "*" ) {
+            println("${num1} ${op} ${num2} = ${num1*num2}")
+        }
+        else if(op == "/" ){
+            println("${num1} ${op} ${num2} = ${num1/num2}")
+        }
+
+
     }
 }
